f0f4d1d757ed455399d2b2bbcb00c0f11ee64ddd
Lalodev <laloing87@gmail.com>
2022-11-18 09:46:45 -0600

    Interface

---
+++ src/hooks/useCalendarStore.js
@@ -0,0 +1,48 @@
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  onAddNewEvent,
+  onDeleteEvent,
+  onSetActiveEvent,
+  onUpdateEvent,
+} from '../store';
+
+export const useCalendarStore = () => {
+  const dispatch = useDispatch();
+  const { events, activeEvent } = useSelector((state) => state.calendar);
+
+  const setActiveEvent = (calendarEvent) => {
+    dispatch(onSetActiveEvent(calendarEvent));
+  };
+
+  const startSavingEvent = async (calendarEvent) => {
+    // todo: llegar al backend
+
+    // todo bien
+
+    if (calendarEvent._id) {
+      //Actualizando
+      dispatch(onUpdateEvent({ ...calendarEvent }));
+    } else {
+      //Creando
+      dispatch(onAddNewEvent({ ...calendarEvent, _id: new Date().getTime() }));
+    }
+  };
+
+  const startDeletingEvent = () => {
+    // Todo: llegar al backend
+
+    dispatch(onDeleteEvent());
+  };
+
+  return {
+    //* Properties
+    events,
+    activeEvent,
+    hasEventSelected: !!activeEvent,
+
+    //* Métodos
+    setActiveEvent,
+    startSavingEvent,
+    startDeletingEvent,
+  };
+};
